62bf19724a56476399d5b6f2b90f54fc1f94ba60
kainister <kevinb@MacBook-Pro-de-Kevin.local>
2019-12-03 18:05:42 +0100

    fix

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+window.onload = function () {
+    var scene = document.querySelector("a-scene");
+
+    console.log("scene")
+    scene.onclick = function () {
+        AFRAME.registerComponent("fly", {
+            init: function() {
+              
+            },
+            tick() {
+              const step = 0.4
+              const player = document.querySelector("a-camera")
+              let r = player.getAttribute('rotation');
+              var pos = player.getAttribute("position");
+              pos.x += step * sin(r.y);
+              pos.y += step * tan(r.x);
+              pos.z += step * cos(r.y);
+              player.setAttribute("position", pos);
+            }
+          })
+    }
+}
+
 function cos(v) {
     return Math.cos((v - 180) * (Math.PI / 180.0));
   }
@@ -12,18 +35,3 @@ function cos(v) {
   }
 
 
-  AFRAME.registerComponent("fly", {
-    init: function() {
-      
-    },
-    tick() {
-      const step = 0.4
-      const player = document.querySelector("a-camera")
-      let r = player.getAttribute('rotation');
-      var pos = player.getAttribute("position");
-      pos.x += step * sin(r.y);
-      pos.y += step * tan(r.x);
-      pos.z += step * cos(r.y);
-      player.setAttribute("position", pos);
-    }
-  })
